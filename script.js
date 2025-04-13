c0695d0d5c1f209d262a909cd9c854438eb1dd26
loydskie11 <loyddsaquilon@gmail.com>
2025-04-13 19:15:36 +0800

    added a shuffle button to shuffle the emojis

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ emojis.forEach(emoji => {
     });
 });
 
+function shuffleEmojis() {
+    document.querySelectorAll('.emoji').forEach(emoji => {
+        const delay = (Math.random() * 4).toFixed(2);
+        const top = Math.random() * 90;
+        const left = Math.random() * 90;
+        emoji.style.animationDelay = `${delay}s`;
+        emoji.style.top = `${top}%`;
+        emoji.style.left = `${left}%`;
+    });
+};
+
+window.addEventListener('load', shuffleEmojis);
+
 window.addEventListener('click', (e) => {
     if (e.target == modal) {
         modal.style.display = 'none';
@@ -23,14 +36,6 @@ closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 
-document.querySelectorAll('.emoji').forEach(emoji => {
-    const delay = (Math.random() * 4).toFixed(2);
-    const top = Math.random() * 90;
-    const left = Math.random() * 90;
-    emoji.style.animationDelay = `${delay}s`;
-    emoji.style.top = `${top}%`;
-    emoji.style.left = `${left}%`;
-});
 
 icon.addEventListener('click', () => {
     if (audio.paused) {
